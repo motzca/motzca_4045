539091821fc2a5b189122b0baf1d5d1c3cda4fa3
motzca <finnickodair11@gmail.com>
2023-02-01 19:20:24 -0500

    Adding comments

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PlantPlacesController {
-
+	
+// Beginner controller
 	@RequestMapping("/start")
 	public String start() {
 		return "start";
